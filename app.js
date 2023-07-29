940f7e70dc31949e5a469ef1042e8d8594895ce2
christopherflores9312 <christopherflores9312@gmail.com>
2023-07-28 18:49:25 -0700

    removed media files and changed url

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var speechTranslation = require("./SpeechTranslation");
 var textToSpeech = require("./Text2Speech");
 var videoProcessor = require("./videoProcessor");
 
-videoProcessor.downloadVideo('https://youtube.com/shorts/Ss9jt-e9lqA', function(videoFile) {
+videoProcessor.downloadVideo('https://www.youtube.com/shorts/RfAKbiWFLIg', function(videoFile) {
     videoProcessor.extractAudio(videoFile, function(audioFile) {
         speechTranslation(audioFile, textToSpeech);
     });
