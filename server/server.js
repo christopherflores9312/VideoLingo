0413ecd185235f9f55646dac349e301419113c31
christopherflores9312 <christopherflores9312@gmail.com>
2023-07-31 23:13:33 -0700

    commented out schema

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const videoRoutes = require('./routes/video');
-const schema = require('./schema'); // import the schema
+// const schema = require('./schema'); // import the schema
 
 
 app.use(cors());
