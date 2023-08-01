dba9e245526a7ed7ac28b683caa9d0decf4a3571
christopherflores9312 <christopherflores9312@gmail.com>
2023-07-31 21:47:39 -0700

    deleted comment

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -10,8 +10,6 @@ exports.processVideo = async (req, res) => {
                 speechTranslation(audioFile, function(translatedText) {
                     textToSpeech(translatedText, function(translatedAudio) {
                         videoProcessor.addAudioToVideo(videoFile, translatedAudio, function(finalVideo) {
-                            // Here, you would probably want to send the final video file back to the client
-                            // The specifics of this will depend on how you're handling file storage and delivery
                             res.status(200).json({ message: 'Video processed successfully', video: finalVideo });
                         });
                     });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const videoRoutes = require('./routes/video');
+const schema = require('./schema'); // import the schema
+
 
 app.use(cors());
 app.use(express.json());
