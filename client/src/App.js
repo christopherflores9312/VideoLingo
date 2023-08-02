84dbc2c08097c5e0ed824c67eb3ede0a99144d77
billhamilton68 <billhamilton68@gmail.com>
2023-08-01 21:24:23 -0700

    Update UI

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,24 @@ import Footer from './components/Footer';
 import VideoProcessor from './components/VideoProcessor';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
+import YouTubeCard from './components/YouTube';
+import VideoPlayerCard from './components/VideoPlayerCard';
 
 function App() {
   return (
   <Container component="main" maxWidth="lg">
       <CssBaseline />  {/* Normalize CSS */}
       <Header />
-     
-       
+      <div className="App-content">
+      
+      
+      <YouTubeCard />
+      <br />
       <VideoProcessor />
+      <VideoPlayerCard videoSrc="./server/output/output.mp4" />
+
+      </div>
+
       <Footer />
     </Container>
   );
