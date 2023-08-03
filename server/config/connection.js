3002a9c4aa22b5844ad8b253e68fa26c922689c9
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-03 11:08:06 -0700

    creating video lingo collection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/videolingo';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
