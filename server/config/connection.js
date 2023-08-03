fb7ecb5d290e8768cdc937818869b801339e39b5
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-02 23:19:53 -0700

    modified mongodb connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 // Connection string
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 // Create a new MongoClient
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
