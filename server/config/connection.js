0680836a30d31c35845f2f5665e2e14ccafd770b
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-03 14:19:58 -0700

    changed localhost to 127.0.0.1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/videolingo';
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/videolingo';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
