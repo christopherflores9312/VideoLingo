d3165b70e25d136b7ff7eef0980e570b4efe38ab
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-02 21:03:01 -0700

    refactored code to use promises instead of callbacks

--- server/SpeechTranslation.js
+++ server/SpeechTranslation.js
@@ -7,30 +7,35 @@ speechTranslationConfig.speechRecognitionLanguage = "en-US";
 var language = "es"; // Modify to Spanish
 speechTranslationConfig.addTargetLanguage(language);
 
-function fromFile(audioFile, callback) {
-    let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync(audioFile));
-    let translationRecognizer = new sdk.TranslationRecognizer(speechTranslationConfig, audioConfig);
+function fromFile(audioFile) {
+    return new Promise((resolve, reject) => {
+        let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync(audioFile));
+        let translationRecognizer = new sdk.TranslationRecognizer(speechTranslationConfig, audioConfig);
 
-    translationRecognizer.recognizeOnceAsync(result => {
-        switch (result.reason) {
-            case sdk.ResultReason.TranslatedSpeech:
-                callback(result.translations.get(language));
-                break;
-            case sdk.ResultReason.NoMatch:
-                console.log("NOMATCH: Speech could not be recognized.");
-                break;
-            case sdk.ResultReason.Canceled:
-                const cancellation = sdk.CancellationDetails.fromResult(result);
-                console.log(`CANCELED: Reason=${cancellation.reason}`);
+        translationRecognizer.recognizeOnceAsync(result => {
+            translationRecognizer.close();
+            
+            switch (result.reason) {
+                case sdk.ResultReason.TranslatedSpeech:
+                    resolve(result.translations.get(language));
+                    break;
+                case sdk.ResultReason.NoMatch:
+                    console.log("NOMATCH: Speech could not be recognized.");
+                    reject(new Error("Speech could not be recognized."));
+                    break;
+                case sdk.ResultReason.Canceled:
+                    const cancellation = sdk.CancellationDetails.fromResult(result);
+                    console.log(`CANCELED: Reason=${cancellation.reason}`);
 
-                if (cancellation.reason == sdk.CancellationReason.Error) {
-                    console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
-                    console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
-                    console.log("CANCELED: Did you set the speech resource key and region values?");
-                }
-                break;
-        }
-        translationRecognizer.close();
+                    if (cancellation.reason == sdk.CancellationReason.Error) {
+                        console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+                        console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+                        console.log("CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    reject(new Error(`Speech translation canceled: ${cancellation.reason}`));
+                    break;
+            }
+        });
     });
 }
 
