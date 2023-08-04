630ec9b40fe807555f67e9821070db0c3146d302
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-03 22:54:37 -0700

    video sizing fixed

--- client/src/components/YouTube.js
+++ client/src/components/YouTube.js
@@ -9,7 +9,7 @@ function YouTubeCard() {
 
   const handleSearch = async () => {
     const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${searchTerm}&type=video&videoDuration=short&key=${API_KEY}`
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=9&q=${searchTerm}&type=video&videoDuration=short&key=${API_KEY}`
     );
     const data = await response.json();
     setVideoList(data.items);
@@ -28,21 +28,25 @@ function YouTubeCard() {
         <Button variant="contained" color="primary" onClick={handleSearch}>
           Search
         </Button>
-        {videoList.map((video) => (
-          <iframe
-            key={video.id.videoId}
-            width="100%"
-            height="315"
-            src={`https://www.youtube.com/embed/${video.id.videoId}`}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen>
-          </iframe>
-        ))}
+        <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+          {videoList.map((video, index) => (
+            <div key={video.id.videoId || index} style={{flexBasis: '30%', height: '0', paddingBottom: '15%', position: 'relative', marginBottom: '2%'}}>
+              <iframe
+                key={video.id.videoId}
+                style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}
+                src={`https://www.youtube.com/embed/${video.id.videoId}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen>
+              </iframe>
+            </div>
+          ))}
+        </div>
       </CardContent>
     </Card>
   );
+  
 }
 
 export default YouTubeCard;
