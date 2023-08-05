4ec628ce9b0662385b26859aba50f35f8dfa0b88
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-04 20:48:11 -0700

    simplified protected route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,20 +2,13 @@ import React, { useContext } from 'react';
 import { Route, Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 
-const ProtectedRoute = ({ element, requiredLevel, ...rest }) => {
+const ProtectedRoute = ({ element, ...rest }) => {
     const auth = useContext(AuthContext);
     const location = useLocation();
 
-    // If the user is not authenticated, redirect to the sign-in page
+    // If the user is not authenticated, redirect to the login page
     if (!auth.user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-
-    // Check if a required level is specified and compare it with the user's level
-    if (requiredLevel !== undefined && auth.user.level < requiredLevel) {
-        // Redirect to an unauthorized or access denied page, or show a message
-        // Note: Replace "/AccessDenied" with the appropriate route or message
-        return <Navigate to="/signup" replace />;
     }
 
     return <Route {...rest} element={element} />;
