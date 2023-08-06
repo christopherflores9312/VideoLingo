8b3372b76ab4c380f78e78ce286af7914f2aa8d3
billhamilton68 <billhamilton68@gmail.com>
2023-08-06 15:39:50 -0700

    added .emv file to client folder.

--- client/src/components/YouTube.js
+++ client/src/components/YouTube.js
@@ -7,6 +7,8 @@ function YouTubeCard() {
 
   const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
+  console.log("API_KEY Hello:", API_KEY);
+
   const handleSearch = async () => {
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=9&q=${searchTerm}&type=video&videoDuration=short&key=${API_KEY}`
@@ -29,18 +31,18 @@ function YouTubeCard() {
           Search
         </Button>
         <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
-          {videoList.map((video, index) => (
-            <div key={video.id.videoId || index} style={{flexBasis: '30%', height: '0', paddingBottom: '15%', position: 'relative', marginBottom: '2%'}}>
-              <iframe
-                key={video.id.videoId}
-                style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}
-                src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen>
-              </iframe>
-            </div>
+  {videoList && videoList.map((video, index) => (
+    <div key={video.id.videoId || index} style={{flexBasis: '30%', height: '0', paddingBottom: '15%', position: 'relative', marginBottom: '2%'}}>
+      <iframe
+        key={video.id.videoId}
+        style={{position: 'absolute', top: '0', left: '0', width: '100%', height: '100%'}}
+        src={`https://www.youtube.com/embed/${video.id.videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen>
+      </iframe>
+    </div>
           ))}
         </div>
       </CardContent>
