21a68b42b32920600f950da8f84660dae60eaad7
christopherflores9312 <christopherflores9312@gmail.com>
2023-08-07 14:34:21 -0700

    updated apollo for heroku config

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,7 +1,11 @@
 // src/apolloClient.js
 import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink } from '@apollo/client';
 
-const httpLink = createHttpLink({ uri: 'http://localhost:5001/graphql' });
+const SERVER_URL = process.env.NODE_ENV === 'production' 
+                   ? 'https://your-heroku-app-url.com/graphql' 
+                   : 'http://localhost:5001/graphql';
+
+const httpLink = createHttpLink({ uri: SERVER_URL });
 
 const authLink = new ApolloLink((operation, forward) => {
     const token = localStorage.getItem('authToken');
